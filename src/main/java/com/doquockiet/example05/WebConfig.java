a3119a0eb362dcca44b44af9c6f08c91486471e4
DoQuocKiet666 <kietracingboy67@gmail.com>
2026-01-20 17:10:07 +0700

    Update image

--- src/main/java/com/doquockiet/example05/WebConfig.java
+++ src/main/java/com/doquockiet/example05/WebConfig.java
@@ -6,17 +6,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins(
-                    "http://localhost:3000", 
-                    "https://doquockiet-comic.netlify.app", 
-                    "http://localhost:5173"
-                )
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true)
-                .maxAge(3600);
-    }
+@Override
+public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+            .allowedOrigins(
+                "https://doquockiet-comic.netlify.app", // Tên miền mới của bạn
+                "http://localhost:5173"               // Cho phép chạy ở máy local
+            )
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true)
+            .maxAge(3600);
+}
 }
